667e0d76346cee1233a4ee6309a7292c2e0fa7fc
sdew021 <sdewangan021021@gmail.com>
2020-08-05 15:24:30 +0530

    wired api for states

--- app/src/main/java/com/example/clarc/ui/global/GlobalFragment.kt
+++ app/src/main/java/com/example/clarc/ui/global/GlobalFragment.kt
@@ -9,10 +9,15 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.clarc.R
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.textview.MaterialTextView
 import kotlinx.android.synthetic.main.fragment_global.*
+import org.json.JSONObject
 
 class GlobalFragment : Fragment() {
 
@@ -20,6 +25,7 @@ class GlobalFragment : Fragment() {
     private lateinit var globalActive: MaterialTextView
     private lateinit var globalRecovered: MaterialTextView
     private lateinit var globalDeceased: MaterialTextView
+    private lateinit var globalText: MaterialTextView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -31,10 +37,29 @@ class GlobalFragment : Fragment() {
         globalActive = root.findViewById(R.id.globalActive)
         globalRecovered = root.findViewById(R.id.globalRecovered)
         globalDeceased = root.findViewById(R.id.globalDeceased)
+        globalText = root.findViewById(R.id.globalText)
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         globalConfirmed.text = "Confirmed\n0"
         globalActive.text = "Active\n0"
         globalRecovered.text = "Recovered\n0"
         globalDeceased.text = "Deceased\n0"
-        return root
+        globalText.text = "response"
+        val queue = Volley.newRequestQueue(requireContext())
+        val url = "https://api.covid19india.org/data.json"
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                val res = JSONObject(response).getJSONArray("statewise")
+                globalText.text = "Response is: ${res.getJSONObject(0).get("active")}"
+            },
+            Response.ErrorListener { globalText.text = "That didn't work!" })
+
+        queue.add(stringRequest)
     }
 }
--- app/src/main/java/com/example/clarc/ui/india/IndiaFragment.kt
+++ app/src/main/java/com/example/clarc/ui/india/IndiaFragment.kt
@@ -1,15 +1,27 @@
 package com.example.clarc.ui.india
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.clarc.R
+import com.google.android.material.textview.MaterialTextView
+import org.json.JSONObject
 
 class IndiaFragment : Fragment() {
 
+    private lateinit var indiaConfirmed: MaterialTextView
+    private lateinit var indiaActive: MaterialTextView
+    private lateinit var indiaRecovered: MaterialTextView
+    private lateinit var indiaDeceased: MaterialTextView
+    private lateinit var indiaText: MaterialTextView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -17,6 +29,31 @@ class IndiaFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_india, container, false)
+        indiaConfirmed = root.findViewById(R.id.indiaConfirmed)
+        indiaActive = root.findViewById(R.id.indiaActive)
+        indiaRecovered = root.findViewById(R.id.indiaRecovered)
+        indiaDeceased = root.findViewById(R.id.indiaDeceased)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val queue = Volley.newRequestQueue(requireContext())
+        val url = "https://api.covid19india.org/data.json"
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                val res = JSONObject(response).getJSONArray("statewise")
+                res.getJSONObject(0).get("active")
+                indiaConfirmed.text = indiaConfirmed.text.toString() + "\n" + res.getJSONObject(0).get("confirmed").toString()
+                indiaActive.text = indiaActive.text.toString() + "\n" + res.getJSONObject(0).get("active").toString()
+                indiaRecovered.text = indiaRecovered.text.toString() + "\n" + res.getJSONObject(0).get("recovered").toString()
+                indiaDeceased.text = indiaDeceased.text.toString() + "\n" + res.getJSONObject(0).get("deaths").toString()
+            },
+            Response.ErrorListener { Log.d("Resp Error India", "onViewCreated: That didn't work!") })
+
+        queue.add(stringRequest)
+
     }
 }
