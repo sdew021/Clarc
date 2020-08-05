f06d12366f6d9b4466a8c054ccae9380b1a83e7b
sdew021 <sdewangan021021@gmail.com>
2020-08-05 14:21:28 +0530

    renamed dashboard to global and added text views to global fragment

---
+++ app/src/main/java/com/example/clarc/ui/global/GlobalFragment.kt
@@ -0,0 +1,43 @@
+package com.example.clarc.ui.global
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.clarc.R
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textview.MaterialTextView
+import kotlinx.android.synthetic.main.fragment_global.*
+
+class GlobalFragment : Fragment() {
+
+    private lateinit var globalViewModel: GlobalViewModel
+    private lateinit var globalConfirmed: MaterialTextView
+    private lateinit var globalActive: MaterialTextView
+    private lateinit var globalRecovered: MaterialTextView
+    private lateinit var globalDeceased: MaterialTextView
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        globalViewModel =
+                ViewModelProviders.of(this).get(GlobalViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_global, container, false)
+        globalConfirmed = root.findViewById(R.id.globalConfirmed)
+        globalActive = root.findViewById(R.id.globalActive)
+        globalRecovered = root.findViewById(R.id.globalRecovered)
+        globalDeceased = root.findViewById(R.id.globalDeceased)
+        globalConfirmed.text = "Confirmed\n0"
+        globalActive.text = "Active\n0"
+        globalRecovered.text = "Recovered\n0"
+        globalDeceased.text = "Deceased\n0"
+        return root
+    }
+}
--- app/src/main/java/com/example/clarc/ui/global/GlobalViewModel.kt
+++ app/src/main/java/com/example/clarc/ui/global/GlobalViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.clarc.ui.dashboard
+package com.example.clarc.ui.global
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class GlobalViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
